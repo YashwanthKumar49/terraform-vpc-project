8be9a8f6718b8c3c8fb4403b1328cf8211416d34
Yashwanth Kumar Lokrey <59729990+YashwanthKumar49@users.noreply.github.com>
2026-03-30 13:49:53 +0530

    Remove Terraform backend setup instructions
    Removed setup instructions for S3 bucket and DynamoDB table.

--- backend.tf
+++ backend.tf
@@ -1,17 +1,5 @@
 
 
-# BEFORE running terraform init:
-#   1. Create the S3 bucket:   aws s3 mb s3://my-terraform-demo-state-bucket
-#   2. Enable versioning:      aws s3api put-bucket-versioning \
-#                                --bucket my-terraform-state-bucket \
-#                                --versioning-configuration Status=Enabled
-#   3. Create DynamoDB table:  aws dynamodb create-table \
-#                                --table-name terraform-lock \
-#                                --attribute-definitions AttributeName=LockID,AttributeType=S \
-#                                --key-schema AttributeName=LockID,KeyType=HASH \
-#                                --billing-mode PAY_PER_REQUEST
-
-
 terraform {
   backend "s3" {
     bucket         = "my-terraform-demo-state-bucket"   
